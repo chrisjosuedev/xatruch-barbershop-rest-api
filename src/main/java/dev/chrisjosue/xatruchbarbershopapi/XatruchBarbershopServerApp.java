4cfef35a3cd4a8ac03ea2b59ddbd82871a9713e5
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2023-11-12 17:13:32 -0600

    Init Commit

---
+++ src/main/java/dev/chrisjosue/xatruchbarbershopapi/XatruchBarbershopServerApp.java
@@ -0,0 +1,13 @@
+package dev.chrisjosue.xatruchbarbershopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XatruchBarbershopServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XatruchBarbershopServerApp.class, args);
+    }
+
+}
---
+++ src/test/java/dev/chrisjosue/xatruchbarbershopapi/XatruchBarbershopServerAppTests.java
@@ -0,0 +1,13 @@
+package dev.chrisjosue.xatruchbarbershopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XatruchBarbershopServerAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
